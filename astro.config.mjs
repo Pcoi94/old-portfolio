070375551735e09285c9a9a891a9d5cd4d004df2
Pcoi94 <pcoi94.mailpro@gmail.com>
2024-06-13 16:01:57 +0200

    V1.4.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,31 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap()],
-  site: 'https://lukadev.me/'
+  site: 'https://pcoiscript.xyz/',
+  integrations: [
+    tailwind({
+      config: {
+      }
+    }),
+    mdx({
+      extendMarkdownConfig: true,
+      remarkPlugins: [],
+      rehypePlugins: [],
+    }),
+    sitemap({
+      filter: page => page.url !== '/exclude-this-page',
+      serialize: ({ url }) => ({
+        url,
+        lastmod: new Date().toISOString(),
+        changefreq: 'monthly',
+        priority: 0.8,
+      }),
+    })
+  ],
+  build: {
+  },
 });
